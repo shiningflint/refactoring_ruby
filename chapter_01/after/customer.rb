b4d7e0b7195da71df113bd8f6d7f490d2407d88a
Adam <adam.keristoper@gmail.com>
2019-03-14 16:40:16 +0900

    Remove spaces

--- chapter_01/after/customer.rb
+++ chapter_01/after/customer.rb
@@ -12,12 +12,10 @@ class Customer
 
   def statement
     result = "Rental Record for #{@name}\n"
-
     @rentals.each do |element|
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
-
     # add footer lines
     result += "Amount owed is #{total_charge}\n"
     result += "You earned #{total_frequent_renter_points} frequent renter points"
