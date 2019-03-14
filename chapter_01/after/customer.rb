549ccc50062951680b1b66ef36a4b9f1ecf238b6
Adam <adam.keristoper@gmail.com>
2019-03-14 15:50:03 +0900

    Directly use element.charge

--- chapter_01/after/customer.rb
+++ chapter_01/after/customer.rb
@@ -15,8 +15,6 @@ class Customer
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
-      this_amount = element.charge
-
       # add frequent renter points
       frequent_renter_points += 1
       # add bonus for a two day new release rental
@@ -24,8 +22,8 @@ class Customer
         frequent_renter_points += 1
       end
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+      total_amount += element.charge
     end
 
     # add footer lines
