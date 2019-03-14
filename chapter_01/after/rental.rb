48f27f4c8b26345bea03615806a6f7757afa43ed
Adam <adam.keristoper@gmail.com>
2019-03-14 15:35:39 +0900

    Not using instance variable

--- chapter_01/after/rental.rb
+++ chapter_01/after/rental.rb
@@ -8,15 +8,15 @@ class Rental
   def charge
     result = 0
 
-    case @movie.price_code
+    case movie.price_code
     when Movie::REGULAR
       result += 2
-      result += (@days_rented - 2) * 1.5 if @days_rented > 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
     when Movie::NEW_RELEASE
-      result += @days_rented * 3
+      result += days_rented * 3
     when Movie::CHILDRENS
       result += 1.5
-      result += (@days_rented - 3) * 1.5 if @days_rented > 3
+      result += (days_rented - 3) * 1.5 if days_rented > 3
     end
 
     result
