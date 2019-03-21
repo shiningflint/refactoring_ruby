b39319cd44f828c831e1c57335f83514a7986112
Adam Christopher <adam.keristoper@gmail.com>
2019-03-21 09:20:32 +0900

    Getting ready for polymorphism

--- chapter_01/after/movie.rb
+++ chapter_01/after/movie.rb
@@ -3,14 +3,49 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+    # @price = case price_code
+    #   when REGULAR
+    #     RegularPrice.new
+    #   when NEW_RELEASE
+    #     NewReleasePrice.new
+    #   when CHILDRENS
+    #     ChildrensPrice.new
+    # end
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
 
   def frequent_renter_points(days_rented)
     (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
   end
+
+  def charge(days_rented)
+    result = 0
+    case price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
 end
+
+# class RegularPrice
+# end
+#
+# class NewReleasePrice
+# end
+#
+# class ChildrensPrice
+# end
